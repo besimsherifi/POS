5fa9ca739e3e5bbf27663085060afe6a3e41ae3a
besimsherifi <sherifibesim@gmail.com>
2021-12-07 01:30:26 +0100

    commit:issue-6 #6 *added some tables to the home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
+import { orderBy } from 'lodash';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  tables:any[] = [];
+
+  constructor(private dataService: DataService, private router: Router, private afAuth: AngularFireAuth, private db: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.db.collection('tables').valueChanges().subscribe((res) => {
+      this.tables = res
+      console.log(res);
+    });
   }
 
 }
