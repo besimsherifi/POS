7fbe826deecca3703359035d56f4a4a57c4b80d2
besimsherifi <57794056+besimsherifi@users.noreply.github.com>
2022-02-13 22:24:13 +0100

    issue-13 #20 *fixed quantity error (#21)

--- src/app/components/table-detail/table-detail.component.ts
+++ src/app/components/table-detail/table-detail.component.ts
@@ -3,6 +3,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { arrayUnion } from 'firebase/firestore';
 import { Subscription } from 'rxjs';
 import { Table } from 'src/app/models/table-model';
 import { DataService } from 'src/app/services/data.service';
@@ -23,8 +24,9 @@ export class TableDetailComponent implements OnInit, OnDestroy {
   price: any = [];
   order: any = [];
   tempOrder: any = [];
+  total: number = 0;
 
-  // uniqueCount = ["a","b","c","d","d","e","a","b","c","f","g","h","h","h","e","a"];
+  uniqueCount = ["a","b","c","d","d","e","a","b","c","f","g","h","h","h","e","a"];
 
   counts:any = {};
 
@@ -44,15 +46,17 @@ export class TableDetailComponent implements OnInit, OnDestroy {
         this.tempOrder = res[0].order;
       }
       console.log(this.order);
-      
+      if(this.uniqueCount.includes('a')){
+        console.log();
+        
+      }
+     
     }); 
 
 
     this.order.forEach((x:any) => { this.counts[x.meal] = (this.counts[x.meal] || 0) + 1; });
     // console.log(this.counts)
 
-    
-    
 
   }
 
@@ -83,18 +87,101 @@ export class TableDetailComponent implements OnInit, OnDestroy {
     } 
   }
 
-  mealClick(meal: any, price:any, quantity: any){
-    this.tempOrder.push({meal, price});     //kjo perdoret se firebase nuk i pranon rekordet duplikat ose si regjistron
+  mealClick(meal: any, price:any, quantity: number){
+         //kjo perdoret se firebase nuk i pranon rekordet duplikat ose si regjistron
     // console.log(this.tempOrder);
     
-      
-      this.db.collection('tables').doc(this.table[0].propertyId).update({order:(this.tempOrder)})
+    
+    for (let i = 0; i < this.tempOrder.length; i++) {
+      const element = this.tempOrder[i];
+      // if(element.meal == meal){
+      //   this.tempOrder[i].quantity += 1;
+      //   this.db.collection('tables').doc(this.table[0].propertyId).update({order:(this.tempOrder)})
+      // }else{
+        
+      //   this.tempOrder.push({meal,price,quantity})
+      //   this.db.collection('tables').doc(this.table[0].propertyId).update({order:(this.tempOrder)})
 
-      if(this.order){
-        this.order.forEach((x:any) => { this.counts[x.meal] = (this.counts[x.meal] || 0) + 1; });
-      }
-      console.log(this.counts)
+      // }
       
+    }
+
+    // for (let i = 0; i < this.order.length; i++) {
+    //   const element = this.order[i];
+    //   if (element.meal == meal) {
+    //     this.
+    //   }
+      
+    // }
+        this.db.collection('tables').doc(this.table[0].propertyId).update({
+          order:arrayUnion({meal,price,quantity})})
+
+
+        // this.tempOrder.push({meal,price,quantity: quantity +1})
+        // console.log(this.tempOrder,'new method');
+        
+    
+    
+      // if(this.tempOrder.includes(obj)){
+      //   console.log(meal);
+      //   this.tempOrder.push({meal,price,quantity: quantity +1})
+      //   this.db.collection('tables').doc(this.table[0].propertyId).update({order:(quantity +=1)})
+      // }else{
+      //   console.log('seka mealin');
+        
+      //   this.tempOrder.push({meal, price, quantity});
+      //   this.db.collection('tables').doc(this.table[0].propertyId).update({order:(this.tempOrder)})
+      // }
+
+      // for (let i = 0; i < this.tempOrder.length; i++) {
+
+        
+      
+      // if(this.order[i].meal == meal){
+      //   console.log(this.order[i].meal);}else{
+      //     console.log('blank');
+          
+      //   }
+        // this.db.collection('tables').doc(this.table[0].propertyId).update({order:(quantity +=1)})
+      // }else{
+        // console.log('seka mealin');
+         
+        // this.tempOrder.push({meal, price, quantity});
+        // this.db.collection('tables').doc(this.table[0].propertyId).update({order:(this.tempOrder)})
+      // }
+    // }
+
+
+
+
+
+
+
+      // if(this.order){
+      //   this.order.forEach((x:any) => { this.counts[x.meal] = (this.counts[x.meal] || 0) + 1; });
+      // }
+      // console.log(this.counts)
+      
+
+  }
+
+  incrementMeal(meal: any){
+    meal.quantity += 1;
+    console.log(meal);
+    this.total += meal.price
+    console.log(this.total);
+    
+  }
+
+  decrementMeal(meal: any){
+    meal.quantity -= 1;
+    console.log(meal);
+  }
+
+  calculateTotal(){
+    this.order.forEach((element:any) =>  this.total += element.price);
+    console.log(this.total);
+    
 
   }
   
